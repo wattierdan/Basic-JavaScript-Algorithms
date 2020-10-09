43cd511a4316a81bf0d922080b793c55d12866d8
Dan <wattierdan@live.com>
2020-10-09 00:25:10 -0700

    initial commit

---
+++ 01-arithmetic/Solved/arithmetic.js
@@ -0,0 +1,21 @@
+// Fill in the body of each function with the code required
+
+var add = function(num1, num2) {
+   var sum = num1 + num2; 
+   return sum;
+};
+
+var subtract = function(num1, num2) {
+    var sum = num1 - num2;
+    return sum;
+};
+
+var multiply = function(num1, num2) {
+    var sum = num1 * num2;
+    return sum;
+};
+
+var divide = function(num1, num2) {
+    var sum = num1 / num2;
+    return sum;
+};
